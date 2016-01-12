2a0136c015a838dba63382cfb34d3136da0497a9
Justin Pease <justinpease2@gmail.com>
2016-01-12 09:33:01 -0700

    ready for individual configuration

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :, ENV['app_secret'], public_key: ENV['app_key']
+# end
---
+++ db/migrate/20160112160319_add_attributes_to_user.rb
@@ -0,0 +1,9 @@
+class AddAttributesToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :name, :string
+    add_column :users, :uid, :string
+    add_column :users, :screen_name, :string
+    add_column :users, :oauth_token, :string
+    add_column :users, :oauth_token_secret, :string
+  end
+end
